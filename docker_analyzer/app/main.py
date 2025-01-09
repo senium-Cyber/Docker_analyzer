f10802ad47eb8a0eef6ac4845c36b2fff0f51939
TsipacsE <csxtchen@comp.hkbu.edu.hk>
2025-01-09 16:48:08 +0800

    Update main.py

--- docker_analyzer/app/main.py
+++ docker_analyzer/app/main.py
@@ -30,16 +30,24 @@ def analyze_dockerfile():
             return jsonify({"error": "No files uploaded"}), 400
 
         # 创建临时文件夹存储上传的文件
-        folder_path = os.path.join(app.config['UPLOAD_FOLDER'], 'temp_folder')
-        os.makedirs(folder_path, exist_ok=True)
+        base_temp_folder = os.path.join(app.config['UPLOAD_FOLDER'], 'temp_folder')
+        os.makedirs(base_temp_folder, exist_ok=True)
 
         # 保存所有上传的文件
         files = request.files.getlist('folder_files')
+        # 获取上传文件的根目录名（假设上传的是一个单一文件夹）
+        root_folder_name = os.path.commonpath([f.filename for f in files]).split(os.path.sep)[0]
+        folder_path = os.path.join(base_temp_folder, root_folder_name)
+
         for file in files:
             relative_path = file.filename  # 获取相对路径
-            full_path = os.path.join(folder_path, relative_path)
+            full_path = os.path.join(base_temp_folder, relative_path)
             os.makedirs(os.path.dirname(full_path), exist_ok=True)  # 创建子目录
             file.save(full_path)
+
+        # 检查文件夹是否成功保存
+        if not os.path.exists(folder_path):
+            return jsonify({"error": f"Uploaded folder {root_folder_name} not found"}), 400
 
         # 查找 Dockerfile
         all_files = get_files_from_folder(folder_path)
@@ -55,6 +63,9 @@ def analyze_dockerfile():
         # 调用 extract_layers 提取层次信息
         os_layer, language_layer, dependencies_layer = extract_layers(dockerfile_ast)
 
+        # 提取并合并其他依赖项
+        extract_requirements(dependencies_layer, folder_path)
+
         # 组装结果
         result = {
             "OS Layer": os_layer,
@@ -63,13 +74,13 @@ def analyze_dockerfile():
         }
 
         # 清理临时文件夹
-        shutil.rmtree(folder_path)
+        shutil.rmtree(base_temp_folder)
         return jsonify(result)
 
     except Exception as e:
-
-        shutil.rmtree(folder_path, ignore_errors=True)
+        shutil.rmtree(base_temp_folder, ignore_errors=True)
         return jsonify({"error": str(e)}), 500
+
 
 
 if __name__ == '__main__':
